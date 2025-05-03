03ba95abf4029fef032bbb813255dc2afccefc2c
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-03 21:28:55 +0100

    Add SortOrder column to ElementType table
    Introduce a new nullable integer column `SortOrder` in the `ElementType` table. Update the migration file to add and remove this column, and reflect the changes in the model snapshot to ensure the database schema is in sync with the entity configuration.

--- CF.GameEngine/CF.GameEngine.Infrastructure.SqlServer/Migrations/20250503202632_added-sort-order.Designer.cs
+++ CF.GameEngine/CF.GameEngine.Infrastructure.SqlServer/Migrations/20250503202632_added-sort-order.Designer.cs
@@ -4,6 +4,7 @@ using CF.GameEngine.Infrastructure.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CF.GameEngine.Infrastructure.SqlServer.Migrations
 {
     [DbContext(typeof(CFGameEngineDbContext))]
-    partial class CFGameEngineDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250503202632_added-sort-order")]
+    partial class addedsortorder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CF.GameEngine/CF.GameEngine.Infrastructure.SqlServer/Migrations/20250503202632_added-sort-order.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CF.GameEngine.Infrastructure.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedsortorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SortOrder",
+                table: "ElementType",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SortOrder",
+                table: "ElementType");
+        }
+    }
+}
