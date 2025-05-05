eb4a639a8b0e5e50683a3164f40dc69bf858fcd2
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-05 16:01:01 +0100

    Enhance Swagger integration and server document filtering
    Updated `Program.cs` to include new using directives, added `AddHttpContextAccessor()`, and configured Swagger with a new API version. Included `app.Run()` to finalize setup.
    
    Modified `IDFCR.Shared.Http.csproj` to add `Swashbuckle.AspNetCore` package reference.
    
    Introduced `SwaggerGenOptionsExtensions` for custom Swagger document filtering and created `RuntimeServerDocumentFilter` to dynamically add server addresses based on the current HTTP context.

--- CF.GameEngine/Web/CF.GameEngine.Web.Api/Program.cs
+++ CF.GameEngine/Web/CF.GameEngine.Web.Api/Program.cs
@@ -1,16 +1,28 @@
 using CF.GameEngine.Infrastructure.SqlServer.Extensions;
 using CF.GameEngine.Web.Api.Endpoints;
+using IDFCR.Shared.Http.Extensions;
+using IDFCR.Shared.Http.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var services = builder.Services
     .AddSingleton(TimeProvider.System)
     .AddMediatR(c => c.RegisterServicesFromAssemblyContaining<Program>())
     .AddBackendDependencies("GameEngineDb")
-    .AddEndpointsApiExplorer();
+    .AddEndpointsApiExplorer()
+    .AddHttpContextAccessor();
 
 services.AddSwaggerGen(options =>
 {
     options.EnableAnnotations(); // If using [SwaggerOperation] etc.
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "GameEngine API",
+        Version = "v1",
+        Description = "GameEngine API"
+    });
+
+    options.AddRuntimeServerDocumentFilter();
 });
 
 var app = builder.Build();
@@ -30,5 +42,6 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "GameEngine API v1");
     });
 }
+
 
 app.Run();
---
+++ IDFCR/IDFCR.Shared.Http/Extensions/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using IDFCR.Shared.Http.SwaggerGen;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace IDFCR.Shared.Http.Extensions;
+
+public static class SwaggerGenOptionsExtensions
+{
+    /// <summary>
+    /// Adds server addresses to Swagger generation, ensure you register the <see cref="IHttpContextAccessor"/> in your DI container.
+    /// <code>Services.AddHttpContextAccessor();</code>
+    /// </summary>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public static SwaggerGenOptions AddRuntimeServerDocumentFilter(this SwaggerGenOptions options)
+    {
+        options.DocumentFilter<RuntimeServerDocumentFilter>();
+        return options;
+    }
+}
+
