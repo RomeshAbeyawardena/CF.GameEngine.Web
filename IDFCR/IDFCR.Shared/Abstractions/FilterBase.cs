e82ce0ddcb2744cbd2fecf508bd11bc488881cf6
Romesh Abeyawardena <romesh.abeyawardena@concerto.co.uk>
2025-05-06 08:36:33 +0100

    Fix grammar and improve clarity in XML doc comment
    Updated the XML documentation comment for the `MappingNotSupportedException`
    property in the `FilterBase` class. Changes include splitting a single-line
    comment into two lines for better readability and correcting a grammatical
    issue by adding the missing word "is."

--- IDFCR/IDFCR.Shared/Abstractions/FilterBase.cs
+++ IDFCR/IDFCR.Shared/Abstractions/FilterBase.cs
@@ -6,7 +6,8 @@ public abstract class FilterBase<TFilter, TDb> : MappableBase<TFilter>, IFilter<
     where TFilter : IFilter<TFilter>
 {
     /// <summary>
-    /// Gets an instance of a NotSupportedException with a message indicating that mapping is not supported, if this intended to be the lowest level filter available to your application.
+    /// Gets an instance of a NotSupportedException with a message indicating that mapping is not supported, 
+    /// if this is intended to be the lowest level filter available to your application.
     /// </summary>
     protected NotSupportedException MappingNotSupportedException => new("This is a low-level filter, use the constructor parameter to map it as the parameters are read-only in this state");
     protected FilterBase(TFilter? targetFilter = default)
