a0e301680314b324c999dc8e209eb83509cd1412
Romesh Abeyawardena <romesh.abeyawardena@concerto.co.uk>
2025-05-09 16:28:24 +0100

    Add IDFC.Shared.FluentValidation project and core classes
    Added a new project `IDFC.Shared.FluentValidation` to the solution,
    targeting .NET 9.0 with nullable reference types and implicit usings
    enabled. Configured build settings and nested project structure.
    
    Implemented `FluentValidationRequestPreProcessor` for request
    validation and `ExceptionHandler` for handling `ValidationException`
    with support for `IApiResult` responses. Added necessary dependencies
    and `using` directives.

---
+++ IDFCR/IDFC.Shared.FluentValidation/FluentValidationRequestPreProcessor.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using IDFCR.Shared.Abstractions.Results;
+using IDFCR.Shared.Http.Results;
+using MediatR;
+using MediatR.Pipeline;
+
+namespace IDFC.Shared.FluentValidation;
+
+public class FluentValidationRequestPreProcessor<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) 
+    : IRequestPreProcessor<TRequest>
+    where TRequest : notnull, IRequest<TResponse>
+{
+    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        List<ValidationFailure> errors = [];
+        var validationContext = new ValidationContext<TRequest>(request);
+        foreach(var validator in validators)
+        {
+            var result = await validator.ValidateAsync(validationContext, cancellationToken);
+            errors.AddRange(result.Errors);
+        }
+
+        if(errors.Count > 0)
+        {
+            throw new ValidationException(errors);
+        }
+    }
+}
+
+public class ExceptionHandler<TRequest, TResponse, TException> : IRequestExceptionHandler<TRequest, TResponse, TException>
+    where TRequest : notnull, IRequest<TResponse>
+    where TException : Exception
+{
+    public Task Handle(TRequest request, TException exception, RequestExceptionHandlerState<TResponse> state, CancellationToken cancellationToken)
+    {
+        if(exception is ValidationException validationException && typeof(TResponse).IsAssignableFrom(typeof(IApiResult)))
+        {
+            var errors = validationException.Errors
+                .ToDictionary(x => x.PropertyName, x => (object?)x.ErrorMessage);
+
+            var response = Activator.CreateInstance(typeof(UnitResult), exception, UnitAction.None, false) as IApiResult;
+            if(response == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (errors is not null)
+            {
+                response?.AppendMeta(errors);
+            }
+
+            state.SetHandled((TResponse)response!);
+        }
+
+        return Task.CompletedTask;
+    }
+}
