7e656975d137691fef5ae55cd13ff4e31aa69e84
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-09 22:18:19 +0100

    Add XML documentation for AddGenericExceptionHandler
    Enhance the `AddGenericExceptionHandler` method in the
    `FluentValidationExtensions` class with XML comments.
    The documentation clarifies that this method should be
    invoked before `AddMediatr` during service registration,
    while the method's functionality remains unchanged.

--- IDFCR/IDFC.Shared.FluentValidation/Extensions/FluentValidationExtensions.cs
+++ IDFCR/IDFC.Shared.FluentValidation/Extensions/FluentValidationExtensions.cs
@@ -5,6 +5,11 @@ namespace IDFC.Shared.FluentValidation.Extensions;
 
 public static class FluentValidationExtensions
 {
+    /// <summary>
+    /// Add this before you add <see cref="AddMediatr"/> in your service registration
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
     public static IServiceCollection AddGenericExceptionHandler(this IServiceCollection services)
     {
         return services.AddTransient(typeof(IRequestExceptionHandler<,,>), typeof(UnitExceptionHandler<,,>));
