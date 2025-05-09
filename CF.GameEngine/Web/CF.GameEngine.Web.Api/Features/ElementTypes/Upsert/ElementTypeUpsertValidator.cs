2fc7209a41dad4784a233fab4b4eff1a301460b3
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-09 21:29:54 +0100

    Add ElementTypeUpsertValidator for command validation
    Introduces `ElementTypeUpsertValidator` class to validate
    `ElementTypeUpsertCommand` properties. Ensures `ElementType`
    is not null, `Name` is not empty, and `Key` is unique using
    an asynchronous uniqueness check via MediatR.

---
+++ CF.GameEngine/Web/CF.GameEngine.Web.Api/Features/ElementTypes/Upsert/ElementTypeUpsertValidator.cs
@@ -0,0 +1,30 @@
+﻿using CF.GameEngine.Web.Api.Features.ElementTypes.Get;
+using FluentValidation;
+using MediatR;
+
+namespace CF.GameEngine.Web.Api.Features.ElementTypes.Upsert;
+
+public class ElementTypeUpsertValidator : AbstractValidator<ElementTypeUpsertCommand>
+{
+    private readonly IMediator _mediator;
+    public ElementTypeUpsertValidator(IMediator mediator)
+    {
+        _mediator = mediator;
+        RuleFor(x => x.ElementType)
+            .NotNull()
+            .WithMessage("ElementType cannot be null.");
+        RuleFor(x => x.ElementType.Name)
+            .NotEmpty()
+            .WithMessage("ElementType name cannot be empty.");
+        RuleFor(x => x.ElementType.Key)
+            .NotEmpty()
+            .MustAsync(MustBeUnique)
+            .WithMessage("ElementType key must be unique.");
+    }
+
+    private async Task<bool> MustBeUnique(string key, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new ElementTypeFindQuery(Key: key), cancellationToken);
+        return !result.HasValue;
+    }
+}
