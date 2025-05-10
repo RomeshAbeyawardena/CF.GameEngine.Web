4a4b6c3f37f7c7d9f4118107e897fd837f6d8713
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-10 22:21:56 +0100

    wip

--- CF.Authentication/CF.Identity/CF.Identity.Api/Program.cs
+++ CF.Authentication/CF.Identity/CF.Identity.Api/Program.cs
@@ -1,4 +1,8 @@
+using CF.Identity.Infrastructure.SqlServer.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddBackendDependencies("");
 var app = builder.Build();
 
 
