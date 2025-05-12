8186e385408c5141c9ec17c3867876bfd32eacb1
Romesh Abeyawardena <romesh.abeyawardena@concerto.co.uk>
2025-05-12 16:33:34 +0100

    Add GetOneOrDefault<T> extension for IUnitResultCollection
    Introduced a new namespace `IDFCR.Shared.Extensions` and added a
    static class `UnitCollectionExtensions`. Implemented a generic
    extension method `GetOneOrDefault<T>` for `IUnitResultCollection<T>`
    to retrieve a single item with optional filtering and ordering.
    Includes checks for collection success and null results.

---
+++ IDFCR/IDFCR.Shared/Extensions/IUnitCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using IDFCR.Shared.Abstractions.Results;
+
+namespace IDFCR.Shared.Extensions;
+
+public static class UnitCollectionExtensions
+{
+    public static T? GetOneOrDefault<T>(this IUnitResultCollection<T> collection, 
+        Func<T, bool>? predicate = null,
+        Func<IEnumerable<T>, IOrderedEnumerable<T>>? orderedTranform = null) where T : class
+    {
+        if (!collection.IsSuccess || collection.Result is null)
+        {
+            return null;
+        }
+
+        var results = collection.Result;
+
+        if (predicate is not null)
+        {
+            if(orderedTranform is not null)
+            {
+                results = orderedTranform(results);
+            }
+
+            return results.FirstOrDefault(predicate);
+        }
+
+        return results.FirstOrDefault();
+    }
+}
