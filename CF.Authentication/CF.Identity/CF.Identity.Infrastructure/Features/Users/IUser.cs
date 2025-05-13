5fbec56fb0c7a6549294b56a2ae3b04b87e791f3
Romesh Abeyawardena <romesh.abeyawardena@concerto.co.uk>
2025-05-13 12:21:34 +0100

    Refactor user interfaces and query parameter usage
    Updated `UserInfoRequestHandler` to use `accessToken!.UserId`
    in `FindUserQuery` for improved user identification. Refactored
    user-related interfaces: introduced `IUserDetail` with `Username`
    property, moved `Username` from `IUserSummary` to `IUserDetail`,
    and updated `IEditableUser` to inherit from `IUserDetail` and
    explicitly include `EmailAddress` and `HashedPassword`. Removed
    a redundant closing brace in `IUserDetail` for cleanup.

--- CF.Authentication/CF.Identity/CF.Identity.Infrastructure/Features/Users/IUser.cs
+++ CF.Authentication/CF.Identity/CF.Identity.Infrastructure/Features/Users/IUser.cs
@@ -6,7 +6,7 @@ public interface IUser : IEditableUser
 {
 }
 
-public interface IEditableUser : IUserSummary
+public interface IEditableUser : IUserDetail
 {
     string EmailAddress { get; }
     string HashedPassword { get; }
@@ -15,8 +15,12 @@ public interface IEditableUser : IUserSummary
 public interface IUserSummary : IMappable<IUser>, IIdentifer
 {
     Guid ClientId { get; }
-    string Username { get; }
     string? PreferredUsername { get; }
+}
+
+public interface IUserDetail : IUserSummary
+{
+    string Username { get; }
     string Firstname { get; }
     string? MiddleName { get; }
     string LastName { get; }
