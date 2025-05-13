5cb40157647c8ed034848812c9c14cff8db132bc
Romesh Abeyawardena <romesh.abeyawardena@concerto.co.uk>
2025-05-13 10:23:00 +0100

    Refactor Hypermedia classes and update solution structure
    Moved HypermediaApiListResult and HypermediaApiResult to a
    new file (HypermediaApiListResult.cs) for better modularity.
    Updated using directives accordingly. Enhanced solution
    (IDFCR.sln) by adding new projects and configurations,
    reflecting architectural improvements. These changes improve
    code organization, maintainability, and separation of concerns.

--- IDFCR/IDFCR.Shared.Http/Results/ApiResult.cs
+++ IDFCR/IDFCR.Shared.Http/Results/ApiResult.cs
@@ -1,89 +1,10 @@
 ﻿using IDFCR.Shared.Exceptions;
 using IDFCR.Shared.Http.Abstractions;
-using IDFCR.Shared.Http.Links;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Primitives;
 
 namespace IDFCR.Shared.Http.Results;
-
-public record HypermediaApiListResult<T>(IEnumerable<T> RawData, int StatusCode)
-    : ApiResult<IHypermediaCollection<T>>(new HypermediaCollection<T>(RawData), StatusCode)
-{
-    protected override void OnExecuteAsync(HttpContext httpContext)
-    {
-        base.OnExecuteAsync(httpContext);
-        var services = httpContext.RequestServices;
-        var linkBuilders = services.GetServices<ILinkBuilder<T>>();
-        var firstBuilder = linkBuilders.FirstOrDefault();
-
-        if (firstBuilder is not null && Data is not null)
-        {
-            if (linkBuilders.Count() > 1)
-            {
-                firstBuilder.Merge(linkBuilders.Skip(1));
-            }
-
-            foreach (var entry in Data)
-            {
-                var wrapper = entry as Hypermedia<T> ?? throw new InvalidCastException("Not a wrapper of Hypermedia");
-
-                if (entry.Data is not null)
-                {
-                    var links = firstBuilder.Build(
-                        services.GetRequiredService<LinkGenerator>()).GenerateLinks(entry.Data);
-
-                    foreach (var (key, value) in links)
-                    {
-                        if (!wrapper.Links.TryAdd(key, value))
-                        {
-                            wrapper.Links[key] = value;
-                        }
-                    }
-                }
-            }
-        }
-        //if there are no builders processing continues without links
-    }
-}
-
-public record HypermediaApiResult<T>(T? RawData, int StatusCode) : ApiResult<IHypermedia<T>>(new Hypermedia<T>(RawData), StatusCode), IApiResult<IHypermedia<T>>
-{
-    protected override void OnExecuteAsync(HttpContext httpContext)
-    {
-        base.OnExecuteAsync(httpContext);
-        var services = httpContext.RequestServices;
-        var linkBuilders = services.GetServices<ILinkBuilder<T>>();
-        var firstBuilder = linkBuilders.FirstOrDefault();
-
-        if (firstBuilder is not null)
-        {
-            if (linkBuilders.Count() > 1)
-            {
-                firstBuilder.Merge(linkBuilders.Skip(1));
-            }
-
-            if (Data.Data is not null)
-            {
-                var links = firstBuilder.Build(
-                    services.GetRequiredService<LinkGenerator>()).GenerateLinks(Data.Data);
-
-                var wrapper = Data as Hypermedia<T> ?? throw new InvalidCastException("Not a wrapper of Hypermedia");
-
-                foreach (var (key, value) in links)
-                {
-                    if (!wrapper.Links.TryAdd(key, value))
-                    {
-                        wrapper.Links[key] = value;
-                    }
-                }
-            }
-        }
-        //if there are no builders processing continues without links
-    }
-}
-
 
 public record ApiResult<T>(T Data, int StatusCode) : ApiResult(StatusCode), IApiResult<T>
 {
---
+++ IDFCR/IDFCR.Shared.Http/Results/HypermediaApiListResult.cs
@@ -0,0 +1,82 @@
+﻿using IDFCR.Shared.Http.Links;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IDFCR.Shared.Http.Results;
+
+public record HypermediaApiListResult<T>(IEnumerable<T> RawData, int StatusCode)
+    : ApiResult<IHypermediaCollection<T>>(new HypermediaCollection<T>(RawData), StatusCode)
+{
+    protected override void OnExecuteAsync(HttpContext httpContext)
+    {
+        base.OnExecuteAsync(httpContext);
+        var services = httpContext.RequestServices;
+        var linkBuilders = services.GetServices<ILinkBuilder<T>>();
+        var firstBuilder = linkBuilders.FirstOrDefault();
+
+        if (firstBuilder is not null && Data is not null)
+        {
+            if (linkBuilders.Count() > 1)
+            {
+                firstBuilder.Merge(linkBuilders.Skip(1));
+            }
+
+            foreach (var entry in Data)
+            {
+                var wrapper = entry as Hypermedia<T> ?? throw new InvalidCastException("Not a wrapper of Hypermedia");
+
+                if (entry.Data is not null)
+                {
+                    var links = firstBuilder.Build(
+                        services.GetRequiredService<LinkGenerator>()).GenerateLinks(entry.Data);
+
+                    foreach (var (key, value) in links)
+                    {
+                        if (!wrapper.Links.TryAdd(key, value))
+                        {
+                            wrapper.Links[key] = value;
+                        }
+                    }
+                }
+            }
+        }
+        //if there are no builders processing continues without links
+    }
+}
+
+public record HypermediaApiResult<T>(T? RawData, int StatusCode) : ApiResult<IHypermedia<T>>(new Hypermedia<T>(RawData), StatusCode), IApiResult<IHypermedia<T>>
+{
+    protected override void OnExecuteAsync(HttpContext httpContext)
+    {
+        base.OnExecuteAsync(httpContext);
+        var services = httpContext.RequestServices;
+        var linkBuilders = services.GetServices<ILinkBuilder<T>>();
+        var firstBuilder = linkBuilders.FirstOrDefault();
+
+        if (firstBuilder is not null)
+        {
+            if (linkBuilders.Count() > 1)
+            {
+                firstBuilder.Merge(linkBuilders.Skip(1));
+            }
+
+            if (Data.Data is not null)
+            {
+                var links = firstBuilder.Build(
+                    services.GetRequiredService<LinkGenerator>()).GenerateLinks(Data.Data);
+
+                var wrapper = Data as Hypermedia<T> ?? throw new InvalidCastException("Not a wrapper of Hypermedia");
+
+                foreach (var (key, value) in links)
+                {
+                    if (!wrapper.Links.TryAdd(key, value))
+                    {
+                        wrapper.Links[key] = value;
+                    }
+                }
+            }
+        }
+        //if there are no builders processing continues without links
+    }
+}
