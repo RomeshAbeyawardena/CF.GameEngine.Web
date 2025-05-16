786cec5d658c436848fe9b2db7a74f4634178513
Romesh Abeyawardena <romesh.abeyawardena@concerto.co.uk>
2025-05-16 16:07:48 +0100

    Add UserSummaryDto class with mapping functionality
    Introduced a new `UserSummaryDto` class in `UserSummaryDto.cs`
    under the `CF.Identity.Api.Features.User` namespace. The class
    inherits from `MappableBase<IUser>` and implements the
    `IUserSummary` interface. Added public properties: `IsSystem`,
    `ClientId`, `PreferredUsername`, and `Id`. Implemented a
    protected `Source` property to map `IUser` to `UserDto`.
    Overrode the `Map` method to populate properties from an
    `IUser` source object. Added necessary `using` directives.

---
+++ CF.Authentication/CF.Identity/CF.Identity.Api/Features/User/UserSummaryDto.cs
@@ -0,0 +1,27 @@
+﻿using CF.Identity.Infrastructure.Features.Users;
+using IDFCR.Shared.Abstractions;
+
+namespace CF.Identity.Api.Features.User;
+
+public class UserSummaryDto : MappableBase<IUser>, IUserSummary
+{
+    protected override IUser Source => new UserDto { 
+        IsSystem = IsSystem, 
+        ClientId = ClientId, 
+        PreferredUsername = PreferredUsername, 
+        Id = Id 
+    };
+
+    public bool IsSystem { get; set; }
+    public Guid ClientId { get; set; }
+    public string? PreferredUsername { get; set; }
+    public Guid Id { get; set; }
+
+    public override void Map(IUser source)
+    {
+        IsSystem = source.IsSystem;
+        ClientId = source.ClientId;
+        PreferredUsername = source.PreferredUsername;
+        Id = source.Id;
+    }
+}
