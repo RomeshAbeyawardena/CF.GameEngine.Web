c003dda186f3e26ba216a34a638bbb6a559cee79
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-18 23:49:15 +0100

    Fix time range calculation in GetValidatyDateRange
    Updated the `GetValidatyDateRange` method in the `DateTimeOffsetRange` record to subtract one minute from the date instead of one hour, altering the time range represented by the method.

--- IDFCR/IDFCR.Shared/Abstractions/ValidityFilterExtensions.cs
+++ IDFCR/IDFCR.Shared/Abstractions/ValidityFilterExtensions.cs
@@ -4,6 +4,6 @@ public record DateTimeOffsetRange(DateTimeOffset FromValue, DateTimeOffset? ToVa
 {
     public static DateTimeOffsetRange GetValidatyDateRange(DateTimeOffset fromValue, DateTimeOffset? toValue = null)
     {
-        return new(fromValue.Date.AddDays(1).AddHours(-1), toValue.GetValueOrDefault(fromValue));
+        return new(fromValue.Date.AddDays(1).AddMinutes(-1), toValue.GetValueOrDefault(fromValue));
     }
 }
