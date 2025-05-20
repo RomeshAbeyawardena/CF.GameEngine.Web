39fc6bcbfbda68983c3b6844f55137c72d306612
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-20 20:31:16 +0100

    Bump version to 1.0.0.1 and add packaging script
    Updated versioning in `Directory.Build.props` to 1.0.0.1.
    Added `create-package.ps1` to the solution for building,
    testing, and packaging the project with support for local
    and remote deployment paths.

---
+++ IDFCR/create-package.ps1
@@ -0,0 +1,48 @@
+param (
+    [int]$targetPath
+)
+
+enum TargetPath {
+    Local = 1;
+    Remote = 2;
+}
+
+dotnet build
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "❌ Build failed with exit code $LASTEXITCODE"
+    exit $LASTEXITCODE
+} else {
+    Write-Host "✅ Build succeeded!"
+}
+
+dotnet test --no-restore --verbosity normal --no-build
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "❌ Tests failed with exit code $LASTEXITCODE"
+    exit $LASTEXITCODE
+} else {
+    Write-Host "✅ Tests succeeded!"
+}
+
+$deploymentPath = ""
+$versionComponent = 0
+
+switch ([TargetPath]$targetPath) {
+    "Local" {
+        $deploymentPath = "C:\dev\packages\IDFCR"
+        $versionComponent = 4
+    }
+    "Remote" {
+        $versionComponent = 3
+    }
+    default {
+        Write-Host "Invalid target path specified. Use 1 for Local or 2 for Remote."
+        exit 1
+    }
+}
+
+./Set-Property-Info "$PSScriptRoot/Directory.Build.props" $versionComponent
+
+dotnet pack -c Release -o $deploymentPath
+
