e40f5fba7f0650b7775148a27cb43bd01f78433e
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-20 22:21:01 +0100

    Update package versions and refactor request handling
    This commit updates the version numbers of several package references from `1.0.0.1` to `1.0.0.2`, including `IDFCR.Shared.FluentValidation`, `IDFCR.Shared.Http`, `IDFCR.Shared.EntityFramework`, `IDFCR.Shared`, and `IDFCR.Utility.Shared`. The versioning in `Directory.Build.props` has also been updated accordingly.
    
    Additionally, the `RoleRequirementPrequestHandler.cs` file has been refactored to implement `IPipelineBehavior<TRequest, TResponse>` instead of `MediatR.Pipeline.IRequestPreProcessor<TRequest>`. This change modifies the method signature from `Process` to `Handle`, reflecting a new approach to request processing within the MediatR pipeline.

--- IDFCR/IDFCR.Shared.Http/Mediatr/RoleRequirementPrequestHandler.cs
+++ IDFCR/IDFCR.Shared.Http/Mediatr/RoleRequirementPrequestHandler.cs
@@ -8,17 +8,17 @@ using Microsoft.Extensions.Logging;
 namespace IDFCR.Shared.Http.Mediatr;
 
 public class RoleRequirementPrequestHandler<TRequest, TResponse>(ILogger<RoleRequirementPrequestHandler<TRequest, TResponse>> logger, 
-    IHttpContextAccessor contextAccessor, IScopedStateReader scopedStateReader) : MediatR.Pipeline.IRequestPreProcessor<TRequest>
+    IHttpContextAccessor contextAccessor, IScopedStateReader scopedStateReader) : IPipelineBehavior<TRequest, TResponse>
     where TRequest : notnull, IRequest<TResponse>, IRoleRequirement
     where TResponse : class
 {
-    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        if(await scopedStateReader.IsScopedBypassInvokedAsync(cancellationToken) || request.Bypass)
+        if (await scopedStateReader.IsScopedBypassInvokedAsync(cancellationToken) || request.Bypass)
         {
-            logger.LogWarning("Bypassing role requirement for request {RequestType}, ensure this was not used by a front-end facing endpoint that required authorisation", 
+            logger.LogWarning("Bypassing role requirement for request {RequestType}, ensure this was not used by a front-end facing endpoint that required authorisation",
                 request.GetType().Name);
-            return;
+            return await next(cancellationToken);
         }
 
         var context = contextAccessor.HttpContext ?? throw new InvalidOperationException("This is not running in a valid HttpContext");
@@ -31,12 +31,12 @@ public class RoleRequirementPrequestHandler<TRequest, TResponse>(ILogger<RoleReq
         }
 
         var roles = request.Roles;
-        if(roles is null || !roles.Any())
+        if (roles is null || !roles.Any())
         {
-            return;
+            return await next(cancellationToken);
         }
 
-        if(request.RoleRequirementType == RoleRequirementType.All
+        if (request.RoleRequirementType == RoleRequirementType.All
             && !roles.All(user.IsInRole))
         {
             throw new UnauthorizedAccessException("User is not authorised");
@@ -47,6 +47,8 @@ public class RoleRequirementPrequestHandler<TRequest, TResponse>(ILogger<RoleReq
         {
             throw new UnauthorizedAccessException("User is not authorised");
         }
+
+        return await next(cancellationToken);
     }
 }
 
