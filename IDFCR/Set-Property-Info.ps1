86e29dbff6c3607aa7c826543232164ba7c8c93d
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-20 20:09:41 +0100

    Downgrade versioning and enhance version update script
    Updated `Directory.Build.props` to change versioning from `1.2.3` to `1.0.0.0`, including `AssemblyVersion`, `FileVersion`, and `InformationalVersion`.
    
    Modified `IDFCR.sln` to add `ExtensibilityGlobals` section with `SolutionGuid`.
    
    Enhanced `Set-Property-Info.ps1` script to include a new `$versionElement` parameter for specifying which version component to increment, along with an enumeration `VersionComponent`. The script now checks for the property file's existence, reads the version, and updates it accordingly.

--- IDFCR/Set-Property-Info.ps1
+++ IDFCR/Set-Property-Info.ps1
@@ -1,3 +1,53 @@
 param (
-    [string]$propFile
+    [string]$propFile,
+    [int]$versionElement
 )
+
+enum VersionComponent {
+    Major = 1
+    Minor = 2
+    Build = 3
+    Revision = 4
+}
+
+
+# Check if the property file exists
+if (-not (Test-Path $propFile)) {
+    Write-Host "Property file not found: $propFile"
+    exit 1
+}
+
+# Read the property file
+[System.Xml.XmlDocument]$xmlDoc = New-Object System.Xml.XmlDocument
+$xmlDoc.Load($propFile)
+$properties = $xmlDoc.SelectSingleNode("Project/PropertyGroup")
+
+[System.Version]$version = [System.Version]::Parse($properties.Version)
+
+switch ([VersionComponent]$versionElement) {
+    "Major" {
+        $version = [System.Version]::new($version.Major + 1, 0, 0, 0)
+    }
+    "Minor" {
+        $version = [System.Version]::new($version.Major, $version.Minor + 1, 0, 0)
+    }
+    "Build" {
+        $version = [System.Version]::new($version.Major, $version.Minor, $version.Build + 1, 0)
+    }
+    "Revision" {
+        $version = [System.Version]::new($version.Major, $version.Minor, $version.Build, $version.Revision + 1)
+    }
+    default {
+        Write-Host "Invalid version element specified. Use 0 for Major, 1 for Minor, 2 for Build, or 3 for Revision."
+        exit 1
+    }
+}
+
+
+$properties.Version = $version.ToString()
+$properties.AssemblyVersion = $version.ToString()
+$properties.FileVersion = $version.ToString()
+$properties.InformationalVersion = $version.ToString()
+
+$xmlDoc.Save($propFile)
+Write-Host "Updated version to $($version.ToString()) in $propFile"
