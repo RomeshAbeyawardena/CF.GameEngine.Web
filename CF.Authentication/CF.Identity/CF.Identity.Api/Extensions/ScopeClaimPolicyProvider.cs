275424791ae665e1d386c303e4cb9925755b5bb3
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-20 22:51:37 +0100

    Add scope-based authorization policies and handler
    This commit introduces a new authorization mechanism with the addition of `ScopeClaimPolicy` and `ScopeClaimPolicyHandler`. The `ScopeClaimPolicy` implements `IAuthorizationRequirement` to enforce scope claims, while the handler checks user roles against these claims.
    
    A new `ScopeClaimPolicyProvider` is also added, implementing `IAuthorizationPolicyProvider`. It defines default and fallback policies that utilize the `ScopeClaimPolicy` and provides a method to retrieve policies based on policy names. Asynchronous methods are used for potential I/O operations, although they currently complete without additional processing.

---
+++ CF.Authentication/CF.Identity/CF.Identity.Api/Extensions/ScopeClaimPolicyProvider.cs
@@ -0,0 +1,42 @@
+﻿using CF.Identity.Api.Features;
+using Microsoft.AspNetCore.Authorization;
+namespace CF.Identity.Api.Extensions;
+
+public record ScopeClaimPolicy(string Scope) : IAuthorizationRequirement;
+
+public class ScopeClaimPolicyHandler : AuthorizationHandler<ScopeClaimPolicy>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ScopeClaimPolicy requirement)
+    {
+        // Check if the user has the required scope claim
+        if (context.User.IsInRole(requirement.Scope))
+        {
+            context.Succeed(requirement);
+        }
+
+        context.Fail();
+        return Task.CompletedTask;
+    }
+}
+
+public class ScopeClaimPolicyProvider : IAuthorizationPolicyProvider
+{
+    private readonly AuthorizationPolicy Default = new([new ScopeClaimPolicy(Roles.GlobalRead)], ["ClientBearer"]);
+    public async Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+    {
+        await Task.CompletedTask;
+        return Default;
+    }
+
+    public async Task<AuthorizationPolicy?> GetFallbackPolicyAsync()
+    {
+        await Task.CompletedTask;
+        return Default;
+    }
+
+    public async Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+    {
+        await Task.CompletedTask;
+        return new ([new ScopeClaimPolicy(policyName)], ["ClientBearer"]);
+    }
+}
