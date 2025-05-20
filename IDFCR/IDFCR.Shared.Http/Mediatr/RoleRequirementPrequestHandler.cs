b24c78c68345eebbfea2f87e508a52e46bd8fc13
Romesh Abeyawardena <romesh.abeyawardena@concerto.co.uk>
2025-05-20 09:14:41 +0100

    Add role-based request preprocessor for MediatR
    Introduced `RoleRequirementPrequestHandler` to handle role-based
    authorization for requests implementing `IRoleRequirement`. Added
    `IRoleRequirement` interface with `Roles` and `RoleRequirementType`
    properties, and a `RoleRequirementType` enum (`None`, `All`, `Some`)
    to define role-checking logic. Throws `UnauthorizedAccessException`
    if user authentication or role requirements are not met. Updated
    namespaces and `using` directives accordingly.

---
+++ IDFCR/IDFCR.Shared.Http/Mediatr/RoleRequirementPrequestHandler.cs
@@ -0,0 +1,42 @@
+﻿using IDFCR.Shared.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace IDFCR.Shared.Http.Mediatr;
+
+public class RoleRequirementPrequestHandler<TRequest, TResponse>(IHttpContextAccessor contextAccessor) : MediatR.Pipeline.IRequestPreProcessor<TRequest>
+    where TRequest : IRequest<TResponse>, IRoleRequirement
+    where TResponse : class
+{
+    public Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        var context = contextAccessor.HttpContext ?? throw new InvalidOperationException("This is not running in a valid HttpContext");
+        var user = context.User;
+        var identity = user.Identity;
+        if (identity is null || !identity.IsAuthenticated)
+        {
+            throw new UnauthorizedAccessException("User is not authenticated");
+        }
+
+        var roles = request.Roles;
+        if(roles is null || !roles.Any())
+        {
+            return Task.CompletedTask;
+        }
+
+        if(request.RoleRequirementType == RoleRequirementType.All
+            && !roles.All(user.IsInRole))
+        {
+            throw new UnauthorizedAccessException("User is not authorised");
+        }
+
+        if (request.RoleRequirementType == RoleRequirementType.Some
+            && !roles.Any(user.IsInRole))
+        {
+            throw new UnauthorizedAccessException("User is not authorised");
+        }
+
+        return Task.CompletedTask;
+    }
+}
+
---
+++ IDFCR/IDFCR.Shared/Abstractions/IRoleRequirement.cs
@@ -0,0 +1,14 @@
+﻿namespace IDFCR.Shared.Abstractions;
+
+public enum RoleRequirementType
+{
+    None = 0,
+    All = 1,
+    Some = 2
+}
+
+public interface IRoleRequirement
+{
+    IEnumerable<string> Roles { get; }
+    RoleRequirementType RoleRequirementType { get; }
+}
