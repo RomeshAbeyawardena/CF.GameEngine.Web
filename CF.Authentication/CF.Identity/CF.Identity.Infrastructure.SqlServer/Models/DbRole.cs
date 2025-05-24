5f437f3344355f464b33bccbe76a1ad053d26d6b
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-24 17:34:26 +0100

    Add DbRole, DbRoleScope, and DbUserRole classes
    Introduce new database entity classes: DbRole, DbRoleScope, and DbUserRole.
    DbRole implements the IRole interface with properties for ClientId, Key,
    DisplayName, and Id, along with a mapping method. DbRoleScope and
    DbUserRole implement the IIdentifer interface, establishing relationships
    to roles and scopes, with DbUserRole containing a collection of
    DbRoleScope instances. Update IRole interface to define essential
    role properties.

---
+++ CF.Authentication/CF.Identity/CF.Identity.Infrastructure.SqlServer/Models/DbRole.cs
@@ -0,0 +1,45 @@
+﻿using CF.Identity.Infrastructure.Features.Roles;
+using IDFCR.Shared.Abstractions;
+
+namespace CF.Identity.Infrastructure.SqlServer.Models;
+
+public class DbRole : MappableBase<IRole>, IRole
+{
+    protected override IRole Source => this;
+
+    public Guid ClientId { get; set; }
+    public string Key { get; set; } = null!;
+    public string? DisplayName { get; set; }
+    public Guid Id { get; set; }
+
+    public override void Map(IRole source)
+    {
+        Id = source.Id;
+        ClientId = source.ClientId;
+        Key = source.Key;
+        DisplayName = source.DisplayName;
+    }
+}
+
+//this has no interface because it is not used in the API, only in the database
+public class DbRoleScope : IIdentifer
+{
+    public Guid Id { get; set; }
+    public Guid RoleId { get; set; }
+    public Guid ScopeId { get; set; }
+    public virtual DbRole Role { get; set; } = null!;
+    public virtual DbScope Scope { get; set; } = null!;
+}
+
+//this has no interface because it is not used in the API, only in the database
+public class DbUserRole : IIdentifer
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid RoleId { get; set; }
+
+    public ICollection<DbRoleScope> Scopes { get; set; } = [];
+
+    public virtual DbUser User { get; set; } = null!;
+    public virtual DbRole Role { get; set; } = null!;
+}
---
+++ CF.Authentication/CF.Identity/CF.Identity.Infrastructure/Features/Roles/IRole.cs
@@ -0,0 +1,12 @@
+﻿using CF.Identity.Infrastructure.Features.Users;
+using IDFCR.Shared.Abstractions;
+
+namespace CF.Identity.Infrastructure.Features.Roles;
+
+public interface IRole : IIdentifer
+{
+    //Non-optional, these will belong to a given a client and no-one else
+    Guid ClientId { get; }
+    string Key { get; }
+    string? DisplayName { get; }
+}
