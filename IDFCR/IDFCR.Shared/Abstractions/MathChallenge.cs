b7236279b5e63fb1667ad1841d2c3549e89f6118
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-24 22:32:55 +0100

    Add MathChallenge class with operations and validation
    Introduced a new namespace `IDFCR.Shared.Abstractions` and added an internal enum `Sign` for mathematical operations: Addition, Subtraction, and Multiplication. Implemented the `MathChallenge` class, which generates random challenges and calculates answers. Added methods for challenge representation and answer validation.

---
+++ IDFCR/IDFCR.Shared/Abstractions/MathChallenge.cs
@@ -0,0 +1,44 @@
+﻿namespace IDFCR.Shared.Abstractions;
+
+internal enum Sign
+{
+    Addition = 1,
+    Subtraction = 2,
+    Multiplication = 3
+}
+
+internal class MathChallenge(Random random)
+{
+    public int FirstElement { get; private set; }
+    public int SecondElement { get; private set; }
+    public int Answer { get; private set; }
+    public Sign Sign { get; private set; }
+
+    public void SetChallenge()
+    {
+        FirstElement = random.Next(1, 50);
+        SecondElement = random.Next(1, 50);
+        Sign = (Sign)random.Next(1, 4); // 1 to 3 inclusive
+
+        Answer = Sign switch
+        {
+            Sign.Addition => FirstElement + SecondElement,
+            Sign.Subtraction => FirstElement - SecondElement,
+            Sign.Multiplication => FirstElement * SecondElement,
+            _ => throw new InvalidOperationException("Unholy maths detected.")
+        };
+    }
+
+    public override string ToString() => Sign switch
+    {
+        Sign.Addition => $"What is the sum of {FirstElement} and {SecondElement}?",
+        Sign.Subtraction => $"What is the difference between {FirstElement} and {SecondElement}?",
+        Sign.Multiplication => $"What is the product of {FirstElement} and {SecondElement}?",
+        _ => "Unknown operation"
+    };
+
+    public bool Solve(int answer)
+    {
+        return answer == Answer;
+    }
+}
