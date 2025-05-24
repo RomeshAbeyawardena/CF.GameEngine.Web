26a6d96f7af9e1010bebbfb047fd239f44e2d473
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-24 12:24:59 +0100

    Enhance exception handling in UnitExceptionHandler
    Updated UnitExceptionHandler to utilize a new finalException variable for improved error handling. Introduced ValidationDisplayException to provide detailed validation error information. This new exception class formats error details, including error codes and attempted values, enhancing the clarity of validation feedback.

---
+++ IDFCR/IDFCR.Shared.FluentValidation/ValidationDisplayException.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using IDFCR.Shared.Exceptions;
+using System.Text;
+
+namespace IDFCR.Shared.FluentValidation;
+
+public class ValidationDisplayException(ValidationException validationException) : Exception(validationException.Message, validationException), IExposableException
+{
+
+    string IExposableException.Details { 
+        get
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach(var error in validationException.Errors)
+            {
+                stringBuilder.AppendLine($"Code: {error.ErrorCode}\t{error.PropertyName}: {error.ErrorMessage}. (Actual hour: {error.AttemptedValue})");
+            }
+
+            return stringBuilder.ToString();
+        } 
+    }
+}
