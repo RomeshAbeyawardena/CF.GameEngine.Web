c35bb8803fcca386d851464c872e6a0e06e00d42
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-25 09:43:55 +0100

    Update versioning and enhance role descriptor functionality
    - Bump version to 1.2.0.0 in Directory.Build.props.
    - Add `DisplayName` and `Description` properties to `DefaultRoleDescriptor`.
    - Introduce `IRoleDescriptorBuilder` interface for building role descriptors.
    - Implement `RoleDescriptorBuilder` class for constructing `DefaultRoleDescriptor` instances with additional metadata.

--- IDFCR/IDFCR.Shared/Abstractions/IRoleDescriptor.cs
+++ IDFCR/IDFCR.Shared/Abstractions/IRoleDescriptor.cs
@@ -3,6 +3,8 @@ namespace IDFCR.Shared.Abstractions;
 
 public interface IRoleDescriptor
 {
+    string? DisplayName { get; }
+    string? Description { get; }
     string? Prefix { get; }
     string Key { get; }
     bool IsPrivileged { get; }
