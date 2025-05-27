1e8e91a00fa470ea758a47a5d0fa5568b4626505
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-27 22:26:28 +0100

    Add Authorise class for role-based authorization
    Introduces a new static class `Authorise` in the
    `IDFCR.Http.Authentication.Extensions` namespace.
    The class includes a generic method `Using<T>` that
    creates an `IAuthorizeData` object based on a
    specified `RoleCategory` and additional roles.
    This method ensures type safety by requiring `T`
    to implement the `IRoleRegistrar` interface and
    calls `RoleRegistrar.FlattenedRoles<T>` to
    retrieve the appropriate roles for the
    `AuthorizeAttribute`.

---
+++ IDFCR/IDFCR.Http.Authentication/Extensions/RoleRegistrarExtensions.cs
@@ -0,0 +1,14 @@
+﻿using IDFCR.Shared.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace IDFCR.Http.Authentication.Extensions;
+
+public static class Authorise
+{
+    public static IAuthorizeData Using<T>(RoleCategory category, params string[] additionalRoles)
+        where T : IRoleRegistrar, new()
+    {
+        var roles = RoleRegistrar.FlattenedRoles<T>(category, additionalRoles);
+        return new AuthorizeAttribute(roles);
+    }
+}
