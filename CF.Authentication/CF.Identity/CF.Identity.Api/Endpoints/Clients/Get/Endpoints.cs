a4448e2f53b6fde74152f11af222bcca82d77c28
Romesh Abeyawardena <romesh.abeyawardena@concerto.co.uk>
2025-05-27 16:23:54 +0100

    Update packages, add auth, and target .NET 9.0
    Updated IDFCR package references to version 1.2.0.18 across projects, ensuring consistency and maintainability.
    Added authorization to the `GetClientAsync` endpoint in `Endpoints.cs` using role-based access control.
    Updated target framework to .NET 9.0 and upgraded dependencies in `CF.Identity.Infrastructure.SqlServer.csproj` and `CF.Identity.Infrastructure.csproj`.
    Updated project versioning in `Directory.Build.props` to reflect the new release version 1.2.0.18.
    Removed unused content and re-added updated package references in `CF.Identity.Api.csproj`.
    Performed general maintenance to improve security and maintainability.

--- CF.Authentication/CF.Identity/CF.Identity.Api/Endpoints/Clients/Get/Endpoints.cs
+++ CF.Authentication/CF.Identity/CF.Identity.Api/Endpoints/Clients/Get/Endpoints.cs
@@ -1,8 +1,11 @@
 ﻿using CF.Identity.Api.Features.Clients;
 using CF.Identity.Api.Features.Clients.Get;
+using CF.Identity.Infrastructure.Features.Clients;
+using IDFCR.Shared.Abstractions;
 using IDFCR.Shared.Extensions;
 using IDFCR.Shared.Http.Extensions;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using System.Runtime.CompilerServices;
 
 namespace CF.Identity.Api.Endpoints.Clients.Get;
@@ -24,6 +27,7 @@ public static class Endpoints
             .WithName(nameof(GetClientAsync))
             .Produces<ClientDetailResponse>(StatusCodes.Status200OK)
             .Produces(StatusCodes.Status404NotFound)
+            .RequireAuthorization(new AuthorizeAttribute(string.Join("", RoleRegistrar.List<ClientRoles>())))
             .WithTags(Route.Tag);
         return app;
     }
