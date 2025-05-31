31a9c9276d68419d7be0e9c56a704a80e1fc9ca8
Romesh Abeyawardena <romesh.a@outlook.com>
2025-05-31 11:05:25 +0100

    Update HashWithArgon2 method signature
    Modified the `HashWithArgon2` method in the `IPIIProtection<T>` interface to include a `string salt` parameter, enhancing its security by allowing the use of a separate salt in the hashing process. The method now accepts `byte[] password`, `string salt`, `int length`, and an optional `Action<Argon2>? configure`.

--- IDFCR/IDFCR.Shared/Abstractions/Cryptography/IPIIProtection.cs
+++ IDFCR/IDFCR.Shared/Abstractions/Cryptography/IPIIProtection.cs
@@ -9,7 +9,7 @@ public interface IPIIProtection<T> : IStateBag
     string Hash(HashAlgorithmName algorithmName, string secret, string salt, int length);
     IReadOnlyDictionary<string, IProtectionInfo> Protect(T entry);
     void Unprotect(T entry, IReadOnlyDictionary<string, IProtectionInfo>? protectionData = null);
-    string HashWithArgon2(ArgonVariation argonVariation, byte[] password, int length, Action<Argon2>? configure = null);
+    string HashWithArgon2(ArgonVariation argonVariation, byte[] password, string salt, int length, Action<Argon2>? configure = null);
     string HashWithHmac(string key, string data);
     string HashWithHmac(string data);
     bool VerifyHashUsing(T hashedEntry, Expression<Func<T, string?>> member, string valueToTest);
