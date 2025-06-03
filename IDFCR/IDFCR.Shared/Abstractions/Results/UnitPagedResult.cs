556ec28534c31e5951a0d7559258b1065dd81711
Romesh Abeyawardena <romesh.a@outlook.com>
2025-06-03 20:03:21 +0100

    Update version and enhance UnitPagedResult functionality
    - Bumped version to 1.3.0.24 in Directory.Build.props.
    - Introduced a static class `UnitPagedResult` with a method
      `FromResult` for easier instance creation.
    - Modified `UnitPagedResult<TResult>` to inherit from
      `UnitResultCollection<TResult>` and added properties
      for `PagedQuery` and `TotalRows`.

--- IDFCR/IDFCR.Shared/Abstractions/Results/UnitPagedResult.cs
+++ IDFCR/IDFCR.Shared/Abstractions/Results/UnitPagedResult.cs
@@ -2,7 +2,22 @@
 
 namespace IDFCR.Shared.Abstractions.Results;
 
-public record UnitPagedResult<TResult> : UnitResult<IEnumerable<TResult>>, IUnitPagedResult<TResult>
+public static class UnitPagedResult
+{
+    public static UnitPagedResult<TResult> FromResult<TResult>(
+        IEnumerable<TResult>? result, 
+        int totalRows, 
+        IPagedQuery pagedQuery, 
+        UnitAction action = UnitAction.None, 
+        bool isSuccess = true, 
+        Exception? exception = null, 
+        FailureReason? failureReason = null)
+    {
+        return new UnitPagedResult<TResult>(result, totalRows, pagedQuery, action, isSuccess, exception, failureReason);
+    }
+}
+
+public sealed record UnitPagedResult<TResult> : UnitResultCollection<TResult>, IUnitPagedResult<TResult>
 {
     public IPagedQuery PagedQuery { get; }
     public int TotalRows { get; }
