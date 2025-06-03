dd61e4c742398763cd9a2c6e6c5bf723b4a11c05
Romesh Abeyawardena <romesh.abeyawardena@concerto.co.uk>
2025-06-03 14:32:25 +0100

    Add filtering support to AccessRoleRepository
    Enhanced AccessRoleRepository by introducing filtering capabilities.
    - Added new using directives for required namespaces.
    - Updated class to inherit from RepositoryBase and implement IAccessRoleRepository.
    - Introduced GetAccessRolesAsync method for filtered access role retrieval.
    - Enabled asynchronous filtering and mapping to AccessRoleDto objects.

--- CF.Authentication/CF.Identity/CF.Identity.Infrastructure.SqlServer/Repositories/AccessRoleRepository.cs
+++ CF.Authentication/CF.Identity/CF.Identity.Infrastructure.SqlServer/Repositories/AccessRoleRepository.cs
@@ -1,9 +1,21 @@
 ﻿using CF.Identity.Infrastructure.Features.AccessRoles;
+using CF.Identity.Infrastructure.SqlServer.Filters;
 using CF.Identity.Infrastructure.SqlServer.Models;
+using IDFCR.Shared.Abstractions.Results;
+using Microsoft.EntityFrameworkCore;
 
 namespace CF.Identity.Infrastructure.SqlServer.Repositories;
 
-internal class AccessRoleRepository(TimeProvider timeProvider, CFIdentityDbContext context) 
+internal class AccessRoleRepository(TimeProvider timeProvider, CFIdentityDbContext context)
     : RepositoryBase<IAccessRole, DbAccessRole, AccessRoleDto>(timeProvider, context), IAccessRoleRepository
 {
+    public async Task<IUnitResultCollection<AccessRoleDto>> GetAccessRolesAsync(IAccessRoleFilter filter, CancellationToken cancellationToken)
+    {
+        var queryFilter = new AccessRoleFilter(filter);
+        var query = await Set<DbAccessRole>(filter)
+            .Where(queryFilter.ApplyFilter(Builder))
+            .ToListAsync(cancellationToken);
+
+        return UnitResultCollection.FromResult(query.Select(x => x.Map<AccessRoleDto>()));
+    }
 }
