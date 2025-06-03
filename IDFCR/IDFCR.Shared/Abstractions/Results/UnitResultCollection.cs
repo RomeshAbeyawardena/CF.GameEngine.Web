58ee203eea34341f8d1848fbd492061c1a70c074
Romesh Abeyawardena <romesh.a@outlook.com>
2025-06-03 19:16:57 +0100

    Enhance UnitResultCollection with documentation and refactor
    Added XML documentation to the `FromResult<T>` method for improved clarity.
    Refactored `UnitResultCollection<TResult>` from a record to a class to allow greater flexibility in behavior and state management.

--- IDFCR/IDFCR.Shared/Abstractions/Results/UnitResultCollection.cs
+++ IDFCR/IDFCR.Shared/Abstractions/Results/UnitResultCollection.cs
@@ -2,6 +2,17 @@
 
 public static class UnitResultCollection
 {
+    /// <summary>
+    /// Provides static factory methods for creating and handling collections of unit results, 
+    /// encapsulating operation outcomes, actions performed, and error information for enumerable result sets.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="result">The internal payload an instance of <see cref="UnitResultCollection{T}" /> will consume, 
+    /// <see cref="Enumerable.ToArray{T}(IEnumerable{T})"/> will be called immediatly against the <see cref="IEnumerable{T}"/>
+    /// </param>
+    /// <param name="action"></param>
+    /// <param name="isSuccess"></param>
+    /// <returns></returns>
     public static IUnitResultCollection<T> FromResult<T>(IEnumerable<T>? result, UnitAction action = UnitAction.Get, bool isSuccess = true)
     {
         return new UnitResultCollection<T>(result?.ToArray(), action, isSuccess);
@@ -15,7 +26,8 @@ public static class UnitResultCollection
 
 
 public record UnitResultCollection<TResult>(IEnumerable<TResult>? Result = null, UnitAction Action = UnitAction.Get,
-    bool IsSuccess = true, Exception? Exception = null, FailureReason? FailureReason = null) : UnitResult<IEnumerable<TResult>>(Result, Action, IsSuccess, Exception, FailureReason), IUnitResultCollection<TResult>
+    bool IsSuccess = true, Exception? Exception = null, FailureReason? FailureReason = null) 
+        : UnitResult<IEnumerable<TResult>>(Result, Action, IsSuccess, Exception, FailureReason), IUnitResultCollection<TResult>
 {
     
 }
