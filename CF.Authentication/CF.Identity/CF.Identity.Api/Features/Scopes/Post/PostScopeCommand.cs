e3251d456b89db19d19813b564874590675e2323
Romesh Abeyawardena <romesh.a@outlook.com>
2025-06-06 01:03:50 +0100

    Add commands and handlers for scope management
    Introduce `PostScopeCommand` and `PostScopeCommandHandler` for posting scope data. Implement `UpsertScopeCommand` and `UpsertScopeCommandHandler` for upserting scope data. Add `UpsertScopeValidator` to validate input for the upsert command. Update using directives for code organization and functionality.

--- CF.Authentication/CF.Identity/CF.Identity.Api/Features/Scopes/Post/PostScopeCommand.cs
+++ CF.Authentication/CF.Identity/CF.Identity.Api/Features/Scopes/Post/PostScopeCommand.cs
@@ -1,7 +1,11 @@
-﻿using CF.Identity.Infrastructure.Features;
+﻿using CF.Identity.Api.Features.Scopes.Upsert;
+using CF.Identity.Infrastructure.Features;
 using CF.Identity.Infrastructure.Features.Scope;
+using IDFCR.Shared.Abstractions.Results;
 using IDFCR.Shared.Abstractions.Roles;
+using IDFCR.Shared.EntityFramework;
 using IDFCR.Shared.Mediatr;
+using MediatR;
 
 namespace CF.Identity.Api.Features.Scopes.Post;
 
@@ -9,4 +13,16 @@ public record PostScopeCommand(EditableScopeDto Scope, bool Bypass = false) : IU
 {
     IEnumerable<string> IRoleRequirement.Roles => [SystemRoles.GlobalWrite, ScopeRoles.ScopeWrite];
     RoleRequirementType IRoleRequirement.RoleRequirementType => RoleRequirementType.Some;
+}
+
+public class PostScopeCommandHandler(IMediator mediator, ITransactionalUnitOfWork transactionalUnitOfWork) : IUnitRequestHandler<PostScopeCommand, Guid>
+{
+    public async Task<IUnitResult<Guid>> Handle(PostScopeCommand request, CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(new UpsertScopeCommand(request.Scope, request.Bypass), cancellationToken);
+
+        await transactionalUnitOfWork.SaveChangesAsync(cancellationToken);
+
+        return result;
+    }
 }
---
+++ CF.Authentication/CF.Identity/CF.Identity.Api/Features/Scopes/Upsert/UpsertScopeCommand.cs
@@ -0,0 +1,23 @@
+﻿using CF.Identity.Api.Features.Scopes.Post;
+using CF.Identity.Infrastructure.Features;
+using CF.Identity.Infrastructure.Features.Scope;
+using IDFCR.Shared.Abstractions.Results;
+using IDFCR.Shared.Abstractions.Roles;
+using IDFCR.Shared.Mediatr;
+
+namespace CF.Identity.Api.Features.Scopes.Upsert;
+
+public record UpsertScopeCommand(EditableScopeDto Scope, bool Bypass = false) : IUnitRequest<Guid>, IRoleRequirement
+{
+    IEnumerable<string> IRoleRequirement.Roles => [SystemRoles.GlobalWrite, ScopeRoles.ScopeWrite];
+    RoleRequirementType IRoleRequirement.RoleRequirementType => RoleRequirementType.Some;
+}
+
+public class UpsertScopeCommandHandler(IScopeRepository scopeRepository)
+    : IUnitRequestHandler<UpsertScopeCommand, Guid>
+{
+    public Task<IUnitResult<Guid>> Handle(UpsertScopeCommand request, CancellationToken cancellationToken)
+    {
+        return scopeRepository.UpsertAsync(request.Scope.Map<Infrastructure.Features.Scope.ScopeDto>(), cancellationToken);
+    }
+}
