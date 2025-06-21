302662cc390cb53bb6a0565e02aa7c4fb8718775
Romesh Abeyawardena <romesh.a@outlook.com>
2025-06-21 17:48:09 +0100

    Add methods for creating DateTimeOffsetRange from strings
    Implemented `FromStringArray` to parse date ranges from an
    IEnumerable<string> and validate input, throwing exceptions
    for invalid cases. Added `TryFromStringArray` for safe
    attempts to create a DateTimeOffsetRange, returning a
    boolean to indicate success or failure.

--- IDFCR/IDFCR.Shared/Abstractions/ValidityFilterExtensions.cs
+++ IDFCR/IDFCR.Shared/Abstractions/ValidityFilterExtensions.cs
@@ -6,4 +6,50 @@ public record DateTimeOffsetRange(DateTimeOffset FromValue, DateTimeOffset? ToVa
     {
         return new(fromValue.Date.AddDays(1).AddTicks(-1), toValue.GetValueOrDefault(fromValue));
     }
+
+    public static DateTimeOffsetRange FromStringArray(IEnumerable<string> dateRange, DateTimeOffset defaultDate)
+    {
+        if(dateRange.Count() > 2)
+        {
+            throw new ArgumentOutOfRangeException(nameof(dateRange), "Expected a maximum of two date ranges");
+        }
+
+        DateTimeOffset from = defaultDate.Date,
+            to = defaultDate.Date.AddDays(1).AddMicroseconds(-1);
+
+        var fromDate = dateRange.FirstOrDefault() ?? throw new NullReferenceException("From date is missing");
+        var toDate = dateRange.ElementAtOrDefault(1) ?? throw new NullReferenceException("To date is missing");
+
+        bool hasFromDate = false;
+        if (!string.IsNullOrWhiteSpace(fromDate) && DateTimeOffset.TryParse(fromDate, out var date))
+        {
+            from = date;
+            hasFromDate = true;
+        }
+
+        if (!string.IsNullOrWhiteSpace(toDate) && DateTimeOffset.TryParse(toDate, out date))
+        {
+            to = date;
+        }
+        else if (hasFromDate)
+        {
+            to = from.AddDays(1).AddMicroseconds(-1);
+        }
+
+        return new DateTimeOffsetRange(from, to);
+    }
+
+    public static bool TryFromStringArray(IEnumerable<string> dateRange, DateTimeOffset defaultDate, out DateTimeOffsetRange range)
+    {
+        range = GetValidatyDateRange(defaultDate);
+        try
+        {
+            range = FromStringArray(dateRange, defaultDate);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 }
