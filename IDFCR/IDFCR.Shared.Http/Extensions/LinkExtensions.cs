c8068f02ad7e09ee88592a8afd454cd2e0e8cb45
Romesh Abeyawardena <romesh.a@outlook.com>
2025-08-28 23:03:52 +0100

    Add LinkExtensions class with ToMetaLink method
    Introduces a new static class `LinkExtensions` in the
    `IDFCR.Shared.Http.Extensions` namespace. Adds the
    generic method `ToMetaLink<T>` to convert
    `Shared.Abstractions.ILink<T>` to `Links.ILink<T>`,
    constrained to types implementing `IIdentifer` and
    `INamed`. Includes necessary using directives.

---
+++ IDFCR/IDFCR.Shared.Http/Extensions/LinkExtensions.cs
@@ -0,0 +1,13 @@
+﻿using IDFCR.Shared.Abstractions;
+using IDFCR.Shared.Http.Links;
+
+namespace IDFCR.Shared.Http.Extensions;
+
+public static class LinkExtensions
+{
+    public static Links.ILink<T> ToMetaLink<T>(Shared.Abstractions.ILink<T> link, string method)
+        where T : IIdentifer, INamed
+    {
+        return new Link<T>(typeof(T).Name, method, typeof(T).Name, [x => x.Id]);
+    }
+}
