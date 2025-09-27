3f011076370232d917d15fdf08c5499cc5950d95
Romesh Abeyawardena <romesh.a@outlook.com>
2025-09-27 19:14:06 +0100

    Add FilterValidity extension and update ToMetaLink method
    Updated the `ToMetaLink` method in `LinkExtensions` to include
    a lambda expression `[x => x.Id]` in the `Link<T>` constructor.
    
    Added a new `ExpressionStarterExtensions` class with a
    `FilterValidity` method to apply filtering logic based on
    `IValidityFilter`. This method adjusts queries using
    `ValidFrom` and `ValidTo` properties.
    
    Included necessary `using` directives for `LinqKit`,
    `IDFCR.Shared.Abstractions.Filters`, and the new namespace
    `IDFCR.Shared.Extensions`.

--- IDFCR/IDFCR.Shared.Http/Extensions/LinkExtensions.cs
+++ IDFCR/IDFCR.Shared.Http/Extensions/LinkExtensions.cs
@@ -8,6 +8,7 @@ public static class LinkExtensions
     public static Links.ILink<T> ToMetaLink<T>(Shared.Abstractions.ILink<T> link, string method)
         where T : IIdentifer, INamed
     {
+        
         return new Link<T>(typeof(T).Name, method, typeof(T).Name, [x => x.Id]);
     }
 }
---
+++ IDFCR/IDFCR.Shared/Extensions/ExpressionStarterExtensions.cs
@@ -0,0 +1,24 @@
+﻿using LinqKit;
+using IDFCR.Shared.Abstractions.Filters;
+
+namespace IDFCR.Shared.Extensions;
+
+public static class ExpressionStarterExtensions
+{
+    public static ExpressionStarter<T> FilterValidity<T>(this ExpressionStarter<T> query, IValidityFilter filter)
+        where T : IValidity
+    {
+
+        if (filter.ValidFrom.HasValue)
+        {
+            query = query.And(x => x.ValidFrom <= filter.ValidFrom.Value);
+        }
+
+        if (filter.ValidTo.HasValue)
+        {
+            query = query.And(x => !x.ValidTo.HasValue || x.ValidTo >= filter.ValidTo.Value);
+        }
+
+        return query;
+    }
+}
